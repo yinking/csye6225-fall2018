dc0951684cdd78391288178d7a9b1079eda3c316
Study-is-happy <zhang.zhiyo@husky.neu.edu>
2018-10-03 16:19:14 -0400

    cloudformation

--- infrastructure/aws/scripts/config.sh
+++ infrastructure/aws/scripts/config.sh
@@ -2,4 +2,4 @@
 
 vpccidr=10.0.0.0/16
 subnet_cidr=(10.0.1.0/24 10.0.2.0/24 10.0.3.0/24 10.0.4.0/24 10.0.5.0/24 10.0.6.0/24)
-subnet_zone=(us-east-1a us-east-1b us-east-1c)
+subnet_zone=(us-east-2a us-east-2b us-east-2c)
