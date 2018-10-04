64ad13d5edb0797f7e23018e8c4d7742234a22cf
Rohannson <luo.han1@husky.neu.edu>
2018-10-04 15:07:39 -0400

    Changing test

--- webapp/src/test/java/com/example/demo/DemoApplicationTests.java
+++ webapp/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -16,20 +16,8 @@ import static org.junit.Assert.assertNotNull;
 @SpringBootTest
 public class DemoApplicationTests {
 
-    @Autowired
-    UserRepository studentRepository;
-
     @Test
     public void contextLoads() {
-        for (int i = 1; i <= 100; i++) {
-            User student = new User();
-            student.setUsername("test" + i);
-            student.setPassword(new BCryptPasswordEncoder().encode("test" + i));
-            studentRepository.save(student);
-        }
-        for (int i = 1; i <= 100; i++) {
-            User student = studentRepository.findByUsername("test"+i);
-            assertNotNull(student);
-        }
+
     }
 }
