268712d8d522e88347571fc67358d48adfa69c1d
fourthzi5 <fourthzi5@localhost.localdomain>
2018-10-13 11:06:11 -0400

    yes,due

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,7 +1,6 @@
-. ./config.sh
 if [ -n "$1" ]
 then
-	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-application.json --parameters ParameterKey=Name,ParameterValue=$1"-csye6225-" ParameterKey=Domain,ParameterValue=$Domain
+	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-application.json --parameters ParameterKey=Name,ParameterValue=$1"-csye6225-"
 else
 	echo "input stack name!"
 fi
