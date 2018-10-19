ac2834edbe25a6a21835e218d0dd78ca50e2aa3a
Study-is-happy <zhang.zhiyo@husky.neu.edu>
2018-10-19 19:39:34 -0400

    cicd

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -1,6 +1,7 @@
+. ./config.sh
 if [ -n "$1" ]
 then
-	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=Name,ParameterValue=$1"-csye6225-" --capabilities CAPABILITY_NAMED_IAM
+	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=Name,ParameterValue=$1"-csye6225-" ParameterKey=domain,ParameterValue=$domain ParameterKey=accountNumber,ParameterValue=$accountNumber --capabilities CAPABILITY_NAMED_IAM
 else
 	echo "input stack name!"
 fi
