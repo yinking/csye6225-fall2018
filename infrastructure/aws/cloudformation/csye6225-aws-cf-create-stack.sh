02b80d4243c61d4286e798c8a6ca547ab60d9275
Rohannson <luo.han1@husky.neu.edu>
2018-10-26 20:13:16 -0400

    Changing Variable value stackName

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -1,6 +1,6 @@
 if [ -n "$1" ]
 then
-	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-networking.json --parameters ParameterKey=Name,ParameterValue=$1
+	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-networking.json --parameters ParameterKey=stackName,ParameterValue=$1
 else
 	echo "input stack name!"
 fi
