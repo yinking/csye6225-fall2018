f7da0fddef6febe83f2babf08ce4f29f02f7935c
YingWang <150336414@qq.com>
2018-11-08 12:51:01 -0500

    Get Domain Name from Host Zone

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,7 +1,10 @@
 . ./config.sh
 if [ -n "$1" ]
 then
-	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-application.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=awsBucketName,ParameterValue=$awsBucketName
+    NAME=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text)
+    NAME=${NAME%?}
+    echo $NAME
+	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-application.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=awsBucketName,ParameterValue=$awsBucketName ParameterKey=ParamDomainName,ParameterValue=$NAME
 else
 	echo "input stack name!"
 fi
--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -1,7 +1,11 @@
 . ./config.sh
 if [ -n "$1" ]
 then
-	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=codeDeployS3Bucket,ParameterValue=$codeDeployS3Bucket ParameterKey=accountNumber,ParameterValue=$accountNumber --capabilities CAPABILITY_NAMED_IAM
+
+    NAME=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text)
+    NAME=${NAME%?}
+    echo $NAME
+	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=codeDeployS3Bucket,ParameterValue=$codeDeployS3Bucket ParameterKey=ParamDomainName,ParameterValue=$NAME ParameterKey=accountNumber,ParameterValue=$accountNumber --capabilities CAPABILITY_NAMED_IAM
 else
 	echo "input stack name!"
 fi
