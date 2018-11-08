136ac9f2cf30a437924337d7c43d3b6a21a6ba96
Rohannson <luo.han1@husky.neu.edu>
2018-11-08 13:45:39 -0500

    Modified stack termination shell script

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
@@ -1,8 +1,8 @@
 . ./config.sh
 if [ -n "$1" ]
 then
-	aws cloudformation delete-stack --stack-name $1
 	aws s3 rm s3://$awsBucketName --recursive
+	aws cloudformation delete-stack --stack-name $1
 else
 	echo "input stack name!"
 fi
--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
@@ -1,8 +1,8 @@
 . ./config.sh
 if [ -n "$1" ]
 then
+    aws s3 rm s3://$codeDeployS3Bucket --recursive
 	aws cloudformation delete-stack --stack-name $1
-	aws s3 rm s3://$codeDeployS3Bucket --recursive
 else
 	echo "input stack name!"
 fi
