239daab6003f276a06748ffa260bf5085bbf51c4
YingWang <150336414@qq.com>
2018-11-10 10:32:49 -0500

    hostzone fixed

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -2,10 +2,10 @@
 if [ -n "$1" ]
 then
 
-    NAME=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text)
-    NAME=${NAME%?}
+    domainName=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text)
+    domainName=${domainName%?}
     echo $NAME
-	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=codeDeployS3Bucket,ParameterValue=$codeDeployS3Bucket ParameterKey=ParamDomainName,ParameterValue=$NAME ParameterKey=accountNumber,ParameterValue=$accountNumber --capabilities CAPABILITY_NAMED_IAM
+	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=codeDeployS3Bucket,ParameterValue=$codeDeployS3Bucket ParameterKey=domainName,ParameterValue=$domainName ParameterKey=accountNumber,ParameterValue=$accountNumber --capabilities CAPABILITY_NAMED_IAM
 else
 	echo "input stack name!"
 fi
