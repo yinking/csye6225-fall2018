8c7b162136b537d7a3e0dbea2be3392606706ef5
Study-is-happy <zhang.zhiyo@husky.neu.edu>
2018-11-10 21:07:03 -0500

    t

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,8 +1,9 @@
 . ./config.sh
 if [ -n "$1" ]
 then
-    domainName=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text)
-	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-application.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=awsBucketName,ParameterValue=$awsBucketName ParameterKey=domainName,ParameterValue=$domainName
+    	domainName=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text)
+	domainName=${domainName%?}
+	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-application.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=domainName,ParameterValue=$domainName
 else
 	echo "input stack name!"
 fi
