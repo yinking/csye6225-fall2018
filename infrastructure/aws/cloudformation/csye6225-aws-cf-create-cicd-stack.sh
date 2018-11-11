5ef7444abb9db73f0883dba2e5da5939c895518c
YingWang <150336414@qq.com>
2018-11-10 21:05:36 -0500

    hostzone-terminate

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -4,7 +4,6 @@ then
 
     domainName=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text)
     domainName=${domainName%?}
-    echo $NAME
 	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=codeDeployS3Bucket,ParameterValue=$codeDeployS3Bucket ParameterKey=domainName,ParameterValue=$domainName ParameterKey=accountNumber,ParameterValue=$accountNumber --capabilities CAPABILITY_NAMED_IAM
 else
 	echo "input stack name!"
--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
@@ -1,6 +1,10 @@
 . ./config.sh
 if [ -n "$1" ]
 then
+    domainName=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text)
+    domainName=${domainName%?}
+    str='code-deploy.'
+    codeDeployS3Bucket="$str$domainName"
     aws s3 rm s3://$codeDeployS3Bucket --recursive
 	aws cloudformation delete-stack --stack-name $1
 else
