9b05ad187b9fb00fb61a398180009ccad4c7e3fa
Study-is-happy <zhang.zhiyo@husky.neu.edu>
2018-11-12 17:53:29 -0500

    t

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-ec2-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-ec2-stack.sh
@@ -3,7 +3,7 @@ if [ -n "$1" ]
 then
 	domainName=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text)
     	domainName=${domainName%?}
-	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-ec2.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=domainName,ParameterValue=$domainName ParameterKey=localLocation,ParameterValue=$localLocation ParameterKey=awsAccessKeyId,ParameterValue=$awsAccessKeyId ParameterKey=awsSecretKey,ParameterValue=$awsSecretKey ParameterKey=accountNumber,ParameterValue=$accountNumber --capabilities CAPABILITY_NAMED_IAM
+	aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-ec2.json --parameters ParameterKey=stackName,ParameterValue=$1 ParameterKey=domainName,ParameterValue=$domainName ParameterKey=localLocation,ParameterValue=$localLocation ParameterKey=awsAccessKeyId,ParameterValue=$awsAccessKeyId ParameterKey=awsSecretKey,ParameterValue=$awsSecretKey ParameterKey=accountNumber,ParameterValue=$accountNumber
 else
 	echo "input stack name!"
 fi
